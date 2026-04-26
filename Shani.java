562effc4166a26037f37b3742fe72defb287f802
shani <shani.29897@gmail.com>
2026-04-26 20:35:09 +0300

    first commit

---
+++ Shani.java
@@ -0,0 +1,7 @@
+public class Shani {
+    public static void main(String[] args) {
+        System.out.println("Shani");
+        System.out.println("Shani");
+        System.out.println("Shani");
+    }
+}
