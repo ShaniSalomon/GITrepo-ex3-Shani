94974e4c52974ad2d1e39d4e5e31d75e23feea27
shani <shani.29897@gmail.com>
2026-04-27 04:04:52 +0300

    update shani 2

--- Shani.java
+++ Shani.java
@@ -1,7 +1,7 @@
 public class Shani {
     public static void main(String[] args) {
-        System.out.println("Shani updated 1");
-        System.out.println("Shani updated 1");
-        System.out.println("Shani updated 1");
+        System.out.println("Shani updated 2");
+        System.out.println("Shani updated 2");
+        System.out.println("Shani updated 2");
     }
 }
